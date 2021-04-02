ad9d13a95bfc04fbb3faefd1b5ab8365ff5017a1
François Roget <francois.roget@ingenico.com>
2021-04-02 15:59:28 +0200

    Make permission loading asynchronous

--- src/PermissionProvider/PermissionProvider.tsx
+++ src/PermissionProvider/PermissionProvider.tsx
@@ -3,16 +3,16 @@ import {Permission} from "../Types";
 import PermissionContext from "./PermissionContext";
 
 type Props = {
-    permissions: Permission[]
+    fetchPermission: (p: Permission) => Promise<boolean>
 }
 
 // This provider is intended to be surrounding the whole application.
 // It should receive the users permissions as parameter
-const PermissionProvider: React.FunctionComponent<Props> = ({permissions, children}) => {
+const PermissionProvider: React.FunctionComponent<Props> = ({fetchPermission, children}) => {
 
     // Creates a method that returns whether the requested permission is available in the list of permissions
     // passed as parameter
-    const isAllowedTo = (permission: Permission) => permissions.includes(permission);
+    const isAllowedTo = async (permission: Permission): Promise<boolean> => await fetchPermission(permission);
 
     // This component will render its children wrapped around a PermissionContext's provider whose
     // value is set to the method defined above
