1a5715f7069e723b9d7a40809a078e63ab573f1e
François Roget <francois.roget@ingenico.com>
2021-04-02 15:59:28 +0200

    Make permission loading asynchronous

--- src/PermissionProvider/Restricted.tsx
+++ src/PermissionProvider/Restricted.tsx
@@ -10,8 +10,12 @@ type Props = {
 // This component is meant to be used everywhere a restriction based on user permission is needed
 const Restricted: React.FunctionComponent<Props> = ({to, fallback, children}) => {
 
-    // We "connect" to the provider thanks to the permission hook
-    const allowed = usePermission(to);
+    // We "connect" to the provider thanks to the PermissionContext
+    const [loading, allowed] = usePermission(to);
+
+    if(loading){
+        return <>Loading...</>;
+    }
 
     // If the user has that permission, render the children
     if(allowed){
--- src/PermissionProvider/usePermission.ts
+++ src/PermissionProvider/usePermission.ts
@@ -1,10 +1,18 @@
-import {useContext} from 'react';
+import {useContext, useState} from 'react';
 import PermissionContext from "./PermissionContext";
 import {Permission} from "../Types";
 
 const usePermission = (permission: Permission) => {
+    const [loading, setLoading] = useState(true);
+    const [allowed, setAllowed] = useState<boolean>();
+
     const {isAllowedTo} = useContext(PermissionContext);
-    return isAllowedTo(permission);
+
+    isAllowedTo(permission).then((allowed) => {
+        setLoading(false);
+        setAllowed(allowed);
+    })
+    return [loading, allowed]
 }
 
 export default usePermission;
