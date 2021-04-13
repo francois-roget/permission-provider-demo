c7f2a4d657807b81365291cf54fcaf7fc182d190
François Roget <francois.roget@ingenico.com>
2021-04-13 12:09:41 +0200

    Add permission caching

--- src/PermissionProvider/PermissionProvider.tsx
+++ src/PermissionProvider/PermissionProvider.tsx
@@ -6,13 +6,26 @@ type Props = {
     fetchPermission: (p: Permission) => Promise<boolean>
 }
 
+type PermissionCache = {
+    [key:string]: boolean;
+}
+
 // This provider is intended to be surrounding the whole application.
 // It should receive the users permissions as parameter
 const PermissionProvider: React.FunctionComponent<Props> = ({fetchPermission, children}) => {
 
+    const cache: PermissionCache = {};
+
     // Creates a method that returns whether the requested permission is available in the list of permissions
     // passed as parameter
-    const isAllowedTo = async (permission: Permission): Promise<boolean> => await fetchPermission(permission);
+    const isAllowedTo = async (permission: Permission): Promise<boolean> => {
+        if(Object.keys(cache).includes(permission)){
+            return cache[permission];
+        }
+        const isAllowed = await fetchPermission(permission);
+        cache[permission] = isAllowed;
+        return isAllowed;
+    };
 
     // This component will render its children wrapped around a PermissionContext's provider whose
     // value is set to the method defined above
