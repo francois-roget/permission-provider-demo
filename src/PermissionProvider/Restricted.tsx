29cf7c1806ccace1595bea69d5363a1736c9cf5b
François Roget <francois.roget@ingenico.com>
2021-04-13 13:26:33 +0200

    Add loading component on Restricted

--- src/PermissionProvider/Restricted.tsx
+++ src/PermissionProvider/Restricted.tsx
@@ -5,16 +5,17 @@ import usePermission from "./usePermission";
 type Props = {
     to: Permission;
     fallback?: JSX.Element | string;
+    loadingComponent?: JSX.Element | string;
 };
 
 // This component is meant to be used everywhere a restriction based on user permission is needed
-const Restricted: React.FunctionComponent<Props> = ({to, fallback, children}) => {
+const Restricted: React.FunctionComponent<Props> = ({to, fallback,loadingComponent, children}) => {
 
     // We "connect" to the provider thanks to the PermissionContext
     const [loading, allowed] = usePermission(to);
 
     if(loading){
-        return <>Loading...</>;
+        return <>{loadingComponent}</>;
     }
 
     // If the user has that permission, render the children
